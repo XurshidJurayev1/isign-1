f9aa025a5e3749ec3b8df8a452373e02670c8c30
XurshidJurayev1 <xjurayev99@gmail.com>
2022-10-17 12:41:28 +0500

    add lazy loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import {Route, Routes} from 'react-router-dom';
-import Layout from './layout';
-import Home from './views/home';
-import Tokens from './views/tokens';
-import Invoice from './views/invoice';
-import Debitors from './views/debitors';
-import Monitoring from './views/monitoring';
-import Organizations from './views/organizations';
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import './App.css';
+const Layout = lazy(() => import("./layout"));
+const Home = lazy(() => import("./views/home"));
+const Tokens = lazy(() => import("./views/tokens"));
+const Invoice = lazy(() => import("./views/invoice"));
+const Debitors = lazy(() => import("./views/debitors"));
+const Monitoring = lazy(() => import("./views/monitoring"));
+const Organizations = lazy(() => import("./views/organizations"));
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route path="home" element={<Home />} />
-                <Route path='monitoring' element={<Monitoring />} />
-                <Route path='invoice' element={<Invoice />} />
-                <Route path='generate-lot-id' element={<Invoice />} />
-                <Route path='book-keeping' element={<Invoice />} />
-                <Route path='removed-contracts' element={<Invoice />} />
-                <Route path='monitoring-tokens' element={<Tokens />} />
-                <Route path='debitors' element={<Debitors />} />
-                <Route path='organizations' element={<Organizations />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="home" element={<Home />} />
+        <Route path="invoice">
+          <Route path="list" element={<Home />} />
+        </Route>
+        <Route path="monitoring" element={<Monitoring />} />
+        <Route path="invoice" element={<Invoice />} />
+        <Route path="generate-lot-id" element={<Invoice />} />
+        <Route path="book-keeping" element={<Invoice />} />
+        <Route path="removed-contracts" element={<Invoice />} />
+        <Route path="monitoring-tokens" element={<Tokens />} />
+        <Route path="debitors" element={<Debitors />} />
+        <Route path="organizations" element={<Organizations />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
